171c1b4e75496887ac18f54577c1991617618cfd
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-26 01:18:22 -0800

    adding level + water

---
+++ Assets/WetSand.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WetSand : MonoBehaviour
+{
+
+    [Header("WetSand")]
+    public SpriteRenderer wetSandRenderer;
+    public float fadeDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Lerp_MeshRenderer_Color(wetSandRenderer, fadeDuration, Color.white, Color.clear));
+    }
+
+    private IEnumerator Lerp_MeshRenderer_Color(SpriteRenderer target_SpriteRenderer, float
+       lerpDuration, Color startLerp, Color targetLerp)
+    {
+        float lerpStart_Time = Time.time;
+        float lerpProgress;
+        bool lerping = true;
+        while (lerping)
+        {
+            yield return new WaitForEndOfFrame();
+            lerpProgress = Time.time - lerpStart_Time;
+            if (target_SpriteRenderer != null)
+            {
+                target_SpriteRenderer.material.color = Color.Lerp(startLerp, targetLerp, lerpProgress / lerpDuration);
+            }
+            else
+            {
+                lerping = false;
+            }
+
+
+            if (lerpProgress >= lerpDuration)
+            {
+                lerping = false;
+            }
+        }
+        yield break;
+    }
+
+}
