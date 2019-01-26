419ad136163a158e48940695f72aeb3d2c9b5de6
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-26 13:10:18 -0800

    Revert "oceanPrefab"
    This reverts commit fcf446f7834a64d6e75952b20e7568c4ae13ee4a.

---
+++ Assets/EndGameTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Entered by " + other);
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("PLAYER " + other + " won the game");
+        }
+
+    }
+}
--- Assets/Scripts/Water/WaterMovement.cs
+++ Assets/Scripts/Water/WaterMovement.cs
@@ -3,52 +3,36 @@ using UnityEngine;
 
 public class WaterMovement : MonoBehaviour
 {
-    [Range(0, 5)]
+    public Transform startPos;
+    public Transform endPos;
     public float speed;
-    public float yShift = 1.5f;  // Amount to move left and right from the start point
-    public float xShift = 0f;
-    private Vector3 startV3Pos;
+    public bool comingIn;
 
-    [Header("Wet Sand")]
     public GameObject wetSandPrefab;
-    public bool baseWaterLayer;
-    public bool isGoingOut;
-    private float prevPos;
-
-    void Start()
-    {
-        startV3Pos = transform.position;
-    }
 
     void Update()
     {
-        Vector3 v = startV3Pos;
-        v.y += yShift * Mathf.Sin(Time.time * speed);
-        v.x += xShift * Mathf.Sin(Time.time * speed);
-        transform.position = v;
+        float step = speed * Time.deltaTime;
 
-        if (baseWaterLayer)
+        if (comingIn)
         {
-            if (!isGoingOut)
-            {
-                if (prevPos < transform.position.y)
-                {
-                    isGoingOut = true;
-                    Debug.Log("water is NOW going out");
-                    Instantiate(wetSandPrefab, transform.position, transform.rotation);
-                }
-            }
-
-            if (isGoingOut)
-            {
-                if (prevPos > transform.position.y)
-                {
-                    isGoingOut = false;
-                    Debug.Log("water is NOW going in");
-                }
-            }
-
-            prevPos = v.y;
+            transform.position = Vector3.MoveTowards(transform.position, endPos.position, step);
+        }
+        if (transform.position == endPos.position)
+        {
+            comingIn = false;
+            Instantiate(wetSandPrefab,transform.position, transform.rotation);
+        }
+        if (!comingIn)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos.position, step);
+        }
+        if (transform.position == startPos.position)
+        {
+            comingIn = true;
         }
     }
+
+   
+
 }
