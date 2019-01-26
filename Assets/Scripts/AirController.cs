a4ab81345131a9ab96e9d403090dff6e1f84a4e1
Oliver Smith <991572+all-iver@users.noreply.github.com>
2019-01-26 12:06:31 -0800

    In progress - text mesh, game ready/in-progress states, turtle numbers

--- Assets/Scripts/AirController.cs
+++ Assets/Scripts/AirController.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using NDream.AirConsole;
 using Newtonsoft.Json.Linq;
+using UnityEngine.UI;
 
 public class AirController : MonoBehaviour {
 
+    public struct Player {
+        public Turtle turtle;
+        public bool isReady;
+        public int deviceID; // air console's ID for the device, stays the same if they disconnect and reconnect 
+        public int playerID; // our internal player ID - the turtle number
+        public bool isKeyboard;
+    }
+
+    public enum GameState {
+        Waiting,
+        InProgress
+    }
+    public GameState gameState;
+
     public GameObject playerPrefab;
-    public Dictionary<int, Turtle> players = new Dictionary<int, Turtle> (); 
+    public Dictionary<int, Player> players = new Dictionary<int, Player> (); 
     TrackingCamera cam;
+    public Image instructionsPanel;
+    public TMPro.TMP_Text instructionsText;
+    string code;
+    public float secondsToWaitWhenAllPlayersAreReady = 10;
+    float readyTimer;
 
     void Awake () {
         AirConsole.instance.onMessage += OnMessage;		
@@ -16,14 +36,28 @@ public class AirController : MonoBehaviour {
         AirConsole.instance.onConnect += OnConnect;		
         AirConsole.instance.onDisconnect += OnDisconnect;
         cam = FindObjectOfType<TrackingCamera>();
+        instructionsText.text = "Loading...";
     }
 
+    string GetStatusText() {
+        string join = "\nTo join, go to airconsole.com on your phone and enter code " + code + ".";
+        if (players.Count == 0)
+            return "Waiting for players." + join;
+        if (!AllPlayersAreReady())
+            return "The game will start when everyone taps \"Ready\"!" + join;
+        if (gameState == GameState.Waiting)
+            return "The game will start in " + Mathf.CeilToInt(readyTimer) + " seconds!" + join;
+        return "Make for the waves!" + join;
+    }
+
+    // this is airconsole's ready message, not related to player.isReady
     void OnReady(string code){
+        this.code = code;
         //Since people might be coming to the game from the AirConsole store once the game is live, 
         //I have to check for already connected devices here and cannot rely only on the OnConnect event 
         List<int> connectedDevices = AirConsole.instance.GetControllerDeviceIds();
         foreach (int deviceID in connectedDevices) {
-            AddNewPlayer (deviceID);
+            AddNewPlayer(deviceID);
         }
     }
 
@@ -32,36 +66,71 @@ public class AirController : MonoBehaviour {
     }
 
     void OnDisconnect(int device) {
-        Turtle player = players[device];
+        Player player = players[device];
         if (cam)
-            cam.RemoveCameraTarget(player.transform);
-        Destroy(player.gameObject);
+            cam.RemoveCameraTarget(player.turtle.transform);
+        Destroy(player.turtle.gameObject);
         players.Remove(device);
+        ReMapPlayerIDs();
+    }
+
+    void BeginGame() {
+        gameState = GameState.InProgress;
     }
 
     void Update() {
-        if (Input.GetKeyDown("space")) {
-            AddNewPlayer(-1);
-            players[-1].useKeyboardInput = true;
+        instructionsText.text = GetStatusText();
+        if (Input.GetKeyDown("space"))
+            AddNewPlayer(-1, true);
+        if (AllPlayersAreReady() && gameState == GameState.Waiting) {
+            readyTimer -= Time.deltaTime;
+            if (readyTimer <= 0)
+                BeginGame();
+        } else {
+            readyTimer = secondsToWaitWhenAllPlayersAreReady;
         }
     }
 
-    void AddNewPlayer(int deviceID) {
+    bool AllPlayersAreReady() {
+        foreach (Player p in players.Values)
+            if (!p.isReady)
+                return false;
+        return true;
+    }
+
+    void ReMapPlayerIDs() {
+        AirConsole.instance.SetActivePlayers();
+        foreach (Player player in players.Values) {
+            if (player.isKeyboard)
+                player.turtle.playerID = -1;
+            player.turtle.playerID = AirConsole.instance.ConvertDeviceIdToPlayerNumber(player.deviceID) + 1;
+        }
+    }
+
+    void AddNewPlayer(int deviceID, bool isKeyboard = false) {
         if (players.ContainsKey(deviceID)) {
             return;
         }
         //Instantiate player prefab, store device id + player script in a dictionary
+        Player player = new Player();
         GameObject newPlayer = Instantiate(playerPrefab, transform.position, transform.rotation) as GameObject;
-        players.Add(deviceID, newPlayer.GetComponent<Turtle>());
+        player.turtle = newPlayer.GetComponent<Turtle>();
+        player.isReady = false;
+        player.deviceID = deviceID;
+        player.isKeyboard = isKeyboard;
+        if (isKeyboard)
+            player.turtle.useKeyboardInput = true;
+        players.Add(deviceID, player);
         if (cam)
-            cam.AddCameraTarget(newPlayer.transform);
+            cam.AddCameraTarget(player.turtle.transform);
+        ReMapPlayerIDs();
     }
 
     void OnMessage(int from, JToken data){
         //When I get a message, I check if it's from any of the devices stored in my device Id dictionary
         if (players.ContainsKey(from) && data["action"] != null) {
             //I forward the command to the relevant player script, assigned by device ID
-            players[from].ButtonInput(data["action"].ToString());
+            players[from].turtle.ButtonInput(data["action"].ToString());
         }
     }
 
