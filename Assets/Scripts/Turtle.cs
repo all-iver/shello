bab9b2b4c54a1f16445f1ef8c19ca814cd19aef2
Oliver Smith <991572+all-iver@users.noreply.github.com>
2019-01-26 01:00:11 -0800

    POC turtle movement

---
+++ Assets/Scripts/Turtle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour {
+
+    bool swipedRightSide, swipedLeftSide;
+	Rigidbody2D rb;
+    public float lurchForce = 1;
+    public Transform leftFlipper, rightFlipper;
+
+	void Start (){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void ButtonInput(string input) {
+		switch (input) {
+		case "swipeRightEnd":
+			swipedRightSide = true;
+			break;
+		case "swipeLeftEnd":
+			swipedLeftSide = true;
+			break;
+		}
+	}
+
+    void Update() {
+        if (Input.GetKeyDown("right"))
+            swipedRightSide = true;
+        if (Input.GetKeyDown("left"))
+            swipedLeftSide = true;
+    }
+
+	void FixedUpdate() {
+		if (swipedLeftSide) {
+            Debug.Log(transform.up * lurchForce);
+			rb.AddForceAtPosition(transform.up * lurchForce, leftFlipper.position, ForceMode2D.Impulse);
+        }
+		if (swipedRightSide) {
+            Debug.Log(transform.up * lurchForce);
+			rb.AddForceAtPosition(transform.up * lurchForce, rightFlipper.position, ForceMode2D.Impulse);
+        }
+        swipedLeftSide = swipedRightSide = false;
+	}
+}
