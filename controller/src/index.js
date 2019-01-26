1ba01e5c7567e4cc0aefc2f738ae371da9494c9a
Ryan Kubik <rmkubik@me.com>
2019-01-26 14:34:13 -0800

    LOad tiki font form google

--- controller/src/index.js
+++ controller/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import WebFont from "webfontloader";
 
 import "./styles/main.css";
 import AirConsole from "air-console";
 import App from "./views/App";
+
+WebFont.load({
+  google: {
+    families: ["Bungee Inline", "Londrina Solid"]
+  }
+});
 
 const airconsole = new AirConsole({
   orientation: AirConsole.ORIENTATION_LANDSCAPE
