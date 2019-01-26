14c963ce577248c3f26206ef1045f2c4d70ad7a8
Ryan Kubik <rmkubik@me.com>
2019-01-26 12:25:42 -0800

    Create new ready/unready button

--- controller/src/views/IntroView.jsx
+++ controller/src/views/IntroView.jsx
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import AirConsole from "air-console";
 
 class IntroView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ready: false
+    };
+  }
   render() {
     const { airconsole } = this.props;
 
@@ -9,23 +16,17 @@ class IntroView extends Component {
       <div id="introView">
         <button
           onClick={() => {
-            console.log("ready");
+            this.setState({
+              ready: !this.state.ready
+            });
+
             airconsole.message(AirConsole.SCREEN, {
-              action: `ready`
+              action: this.state.ready ? "unready" : "ready"
             });
           }}
+          className={this.state.ready ? "ready" : "unready"}
         >
-          Ready
-        </button>
-        <button
-          onClick={() => {
-            console.log("unready");
-            airconsole.message(AirConsole.SCREEN, {
-              action: `unready`
-            });
-          }}
-        >
-          Un-Ready
+          Click to {this.state.ready ? "Un-Ready" : "Ready"}!
         </button>
       </div>
     );
