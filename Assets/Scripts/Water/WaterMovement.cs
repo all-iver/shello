b6af8defe6fc40dd12078043c541984512652bbc
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-26 01:48:38 -0800

    git stuff

---
+++ Assets/Scripts/Water/WaterMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+    public float speed;
+    public bool comingIn;
+
+    public GameObject wetSandPrefab;
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        if (comingIn)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos.position, step);
+        }
+        if (transform.position == endPos.position)
+        {
+            comingIn = false;
+            Instantiate(wetSandPrefab,transform.position, transform.rotation);
+        }
+        if (!comingIn)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos.position, step);
+        }
+        if (transform.position == startPos.position)
+        {
+            comingIn = true;
+        }
+    }
+
+   
+
+}
