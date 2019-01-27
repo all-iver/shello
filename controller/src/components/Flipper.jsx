0b03de5fc65b600d0c187f21e0749b29f6b0785c
Ryan Kubik <rmkubik@me.com>
2019-01-27 10:17:05 -0800

    Calculating swipe length

--- controller/src/components/Flipper.jsx
+++ controller/src/components/Flipper.jsx
@@ -2,16 +2,41 @@ import React, { Component } from "react";
 import Swipe from "react-easy-swipe";
 import AirConsole from "air-console";
 
+const distance = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
 class Flipper extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      start: undefined
+    };
+  }
+
   render() {
     const { airconsole, side } = this.props;
 
     return (
       <Swipe
-        onSwipeStart={console.log}
-        onSwipeMove={console.log}
+        onSwipeStart={evt => {
+          this.setState({
+            start: {
+              x: evt.changedTouches[0].pageX,
+              y: evt.changedTouches[0].pageY
+            }
+          });
+        }}
+        // onSwipeMove={console.log}
         onSwipeEnd={evt => {
           evt.preventDefault();
+
+          console.log(
+            distance(this.state.start, {
+              x: evt.changedTouches[0].pageX,
+              y: evt.changedTouches[0].pageY
+            })
+          );
 
           airconsole.message(AirConsole.SCREEN, {
             action: `swipe${side}End`
