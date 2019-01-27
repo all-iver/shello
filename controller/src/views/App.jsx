f08979307d3dc823b16853ebae970671b08f3beb
Ryan Kubik <rmkubik@me.com>
2019-01-27 14:23:28 -0800

    Start controller in intro view

--- controller/src/views/App.jsx
+++ controller/src/views/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      currentView: "PlayView",
+      currentView: "IntroView",
       body: "green",
       number: "?",
       turtleHidden: true
