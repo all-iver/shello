05bc160c46907e6f8d2cf5e33df115b9782deaa5
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-27 13:54:20 -0800

    Crabs!

---
+++ Assets/Scripts/CrabWalker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabWalker : MonoBehaviour
+{
+
+    [Range(0, 5)]
+    public float speed;
+    public float yShift = 1.5f;
+    //move left and right from the start point
+    public float xShift = 0f;
+    private Vector3 startV3Pos;
+
+   
+
+    void Start()
+    {
+        startV3Pos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 v = startV3Pos;
+        v.y += yShift * Mathf.Sin(Time.time * speed);
+        v.x += xShift * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+        
+    }
+
+
+}
