2747e8fa803c316ebabb0a8d1a4ab9e94d27ef10
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-26 22:31:30 -0800

    nest + level + water etc

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    [Header("Axis to rotate around:")]
+    public bool xAxis = false;
+    public bool yAxis = false;
+    public bool zAxis = false;
+
+    void Awake()
+    {
+        if (xAxis == true)
+        {
+            transform.Rotate(Random.Range(0.0f, 360.0f), 0.0f, 0.0f);
+        }
+
+        if (yAxis == true)
+        {
+            transform.Rotate(0.0f, Random.Range(0.0f, 360.0f), 0.0f);
+        }
+
+        if (zAxis == true)
+        {
+            transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+        }
+    }
+}
---
+++ Assets/Scripts/RandomSpriteElements.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomSpriteElements : MonoBehaviour
+{
+    private SpriteRenderer sp;
+
+    [Header("Elements to Randomize:")]
+    public bool randomlyFlipX = false;
+    public bool randomlyFlipY = false;
+
+
+    void Awake()
+    {
+        sp = GetComponent<SpriteRenderer>();
+
+        if (randomlyFlipX == true)
+        {
+            var myBool = (Random.value < 0.5);
+            sp.flipX = myBool;
+            }
+
+        if (randomlyFlipY == true)
+        {
+            var myBool = (Random.value < 0.5);
+            sp.flipY = myBool;
+        }
+    }
+}
