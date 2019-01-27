a6b06ee3e0d938e70fcedf642d9738c42538101e
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-27 15:33:42 -0800

    Water Splash

--- Assets/Scripts/TurtleWaterTrail.cs
+++ Assets/Scripts/TurtleWaterTrail.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class TurtleWaterTrail : MonoBehaviour
 {
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         TurtleTrailManager turtleTrails = other.gameObject.GetComponentInChildren<TurtleTrailManager>();
         if (!turtleTrails)
             return;
         turtleTrails.waterTrail.enabled = true;
+        audioSource.Play();
     }
 
     private void OnTriggerExit2D(Collider2D other)
