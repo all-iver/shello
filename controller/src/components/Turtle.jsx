88b099a76715b5f0b95223a504331ff4f84ce8ab
Ryan Kubik <rmkubik@me.com>
2019-01-26 16:52:34 -0800

    Build turtle out of bits

--- controller/src/components/Turtle.jsx
+++ controller/src/components/Turtle.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
-import turtleBody from "../../assets/images/Turtle_body.png";
+import turtleBody from "../../assets/images/turtle_body_teal.png";
+import turtleLegsLeft from "../../assets/images/Turtle_legs_left 1.png";
+import turtleLegsRight from "../../assets/images/Turtle_legs_right 1.png";
 
 class Egg extends Component {
   render() {
     return (
       <div className="turtle">
-        <img src={turtleBody} />
+        <img src={turtleBody} className="body" />
+        <img src={turtleLegsLeft} className="left-legs" />
+        <img src={turtleLegsRight} className="right-legs" />
       </div>
     );
   }
