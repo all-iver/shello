21650eb8ba437ea6d5c2d80bc8829d8a055a1e1d
Kavan Bahrami <kavanbahrami@gmail.com>
2019-01-26 23:30:18 -0800

    Turtle Water Trail

---
+++ Assets/Scripts/TurtleWaterTrail.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleWaterTrail : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        TurtleTrailManager turtleTrails = other.gameObject.GetComponentInChildren<TurtleTrailManager>();
+        if (!turtleTrails)
+            return;
+        turtleTrails.waterTrail.enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        TurtleTrailManager turtleTrails = other.gameObject.GetComponentInChildren<TurtleTrailManager>();
+        if (!turtleTrails)
+            return;
+        turtleTrails.waterTrail.enabled = false;
+    }
+}
