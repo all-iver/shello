547a23697c241a08e27909afb0f58d614ebd1602
Ryan Kubik <rmkubik@me.com>
2019-01-26 16:17:21 -0800

    Change egg animation and add shadow

--- controller/src/components/Egg.jsx
+++ controller/src/components/Egg.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 import egg from "../../assets/images/egg_1.png";
+import shadow from "../../assets/images/egg_shadow.png";
+
+function pickRandomlyFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 class Egg extends Component {
   constructor(props) {
@@ -21,23 +26,31 @@ class Egg extends Component {
         <img
           src={egg}
           onClick={() => {
+            if (taps >= 15) {
+              ready();
+            }
+
             this.setState(prevState => ({
               wobbling: true,
               taps: prevState.taps + 1
             }));
           }}
-          className={wobbling ? "wobble" : ""}
+          className={
+            wobbling
+              ? pickRandomlyFromArray([
+                  "eggSprite wobble-short-left",
+                  "eggSprite wobble-short-right"
+                ])
+              : "eggSprite"
+          }
           onAnimationEnd={() => {
             this.setState({
               wobbling: false,
               cracks: cracks + 1
             });
-
-            if (this.state.cracks >= 2) {
-              ready();
-            }
           }}
         />
+        <img src={shadow} className="shadow" />
       </div>
     );
   }
